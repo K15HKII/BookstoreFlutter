0af16d8982965b55cfc95cd297890139bbce0c5a
Huytimeclock <20520554@gm.uit.edu.vn>
2022-06-11 01:22:05 +0700

    permission

--- lib/ui/components/permission.dart
+++ lib/ui/components/permission.dart
@@ -1,22 +1,24 @@
 import 'package:bookstore_flutter/ui/components/namechange_card.dart';
-import 'package:bookstore_flutter/ui/components/notificationpanel.dart';
 import 'package:bookstore_flutter/ui/components/password_change_card.dart';
 import 'package:bookstore_flutter/ui/components/address.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:bookstore_flutter/ui/components/permission.dart';
-class Setting2 extends StatefulWidget {
-  const Setting2({Key? key}) : super(key: key);
+
+class Permission1 extends StatefulWidget {
+  const Permission1({Key? key}) : super(key: key);
 
   @override
-  State<Setting2> createState() => _Setting2State();
+  State<Permission1> createState() => _Permission1State();
 }
 
-class _Setting2State extends State<Setting2> {
+class _Permission1State extends State<Permission1> {
   @override
   Widget build(BuildContext context) {
-    bool Switchbackground=false;
+    bool Camera=false;
+    bool Gallery=false;
+    bool Location=false;
+
     return Scaffold(
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(75.0),
@@ -27,7 +29,7 @@ class _Setting2State extends State<Setting2> {
             toolbarHeight: 75.0,
             backgroundColor: Colors.white,
             title: const Text(
-              "Cài đặt",
+              "Riêng tư",
               style: TextStyle(
                   color: Colors.black, fontFamily: 'Lato', fontSize: 24),
               textAlign: TextAlign.center,
@@ -39,6 +41,15 @@ class _Setting2State extends State<Setting2> {
           child: Column(
             children: [
               SizedBox(height: 27,),
+              Padding(padding: EdgeInsets.only(left: 24)
+                ,child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Chọn sự riêng tư bạn muốn cho phép",
+                      style: TextStyle(
+                          fontSize: 12, fontFamily: "Lato"),
+                    )),),
+              SizedBox(height: 16,),
               Container(
                   height: 60,
                   margin: const EdgeInsets.symmetric(horizontal: 10),
@@ -62,205 +73,7 @@ class _Setting2State extends State<Setting2> {
                             child: Align(
                                 alignment: Alignment.centerLeft,
                                 child: Text(
-                                  "Thông báo",
-                                  style: TextStyle(
-                                      fontSize: 16, fontFamily: "Lato"),
-                                ))),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 45),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              NameChangeCard()));
-                                },
-                                child: const Text(
-                                  "",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontFamily: "Lato"),
-                                ),
-                              ),
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 3),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: IconButton(
-                                icon: Icon(Icons.arrow_forward_ios_outlined,
-                                    color: Color(0xff0C5776)),
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              Notification1()));
-                                },
-                              ),
-                            ))
-                      ],
-                    ),
-                  )),
-
-
-              Container(
-                  height: 60,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(10)),
-                    color: const Color(0xffFFFFFF),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 8,
-                        offset: Offset(0, 4), // Shadow position
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Stack(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  "Riêng tư",
-                                  style: TextStyle(
-                                      fontSize: 16, fontFamily: "Lato"),
-                                ))),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 45),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: InkWell(
-                                onTap: () {
-                                  //Navigator.push(
-                                  //context,
-                                  //MaterialPageRoute(
-                                   //builder: (context) => Permission1()));
-                                },
-                                child: const Text(
-                                  "",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontFamily: "Lato"),
-                                ),
-                              ),
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 3),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: IconButton(
-                                icon: Icon(Icons.arrow_forward_ios_outlined,
-                                    color: Color(0xff0C5776)),
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              Permission1()));
-                                },
-                              ),
-                            ))
-                      ],
-                    ),
-                  )),
-              SizedBox(
-                height: 16,
-              ),
-
-              Container(
-                  height: 60,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius:
-                    BorderRadius.vertical(top: Radius.circular(10)),
-                    color: const Color(0xffFFFFFF),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 8,
-                        offset: Offset(0, 4), // Shadow position
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Stack(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  "Ngôn ngữ",
-                                  style: TextStyle(
-                                      fontSize: 16, fontFamily: "Lato"),
-                                ))),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 45),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: InkWell(
-                                onTap: () {},
-                                child: const Text(
-                                  "",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontFamily: "Lato"),
-                                ),
-                              ),
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(right: 3),
-                            child: Align(
-                              alignment: Alignment.centerRight,
-                              child: IconButton(
-                                icon: Icon(Icons.arrow_forward_ios_outlined,
-                                    color: Color(0xff0C5776)),
-                                onPressed: () {
-                                },
-                              ),
-                            ))
-                      ],
-                    ),
-                  )),
-              SizedBox(
-                height: 1,
-              ),
-              Container(
-                  height: 60,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius:
-                    BorderRadius.vertical(bottom: Radius.circular(10)),
-                    color: const Color(0xffFFFFFF),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 8,
-                        offset: Offset(0, 4), // Shadow position
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Stack(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  "Nền tối",
+                                  "Camera",
                                   style: TextStyle(
                                       fontSize: 16, fontFamily: "Lato"),
                                 ))),
@@ -289,14 +102,137 @@ class _Setting2State extends State<Setting2> {
                             child: Align(
                                 alignment: Alignment.centerRight,
                                 child: CupertinoSwitch(
-                                 value: Switchbackground ,
+                                  value: Camera ,
                                   onChanged:(bool blackscreenvalue){
-                                  //khong biet cach setstate
+                                    //khong biet cach setstate
                                   } ,
                                 )))
                       ],
                     ),
                   )),
+              SizedBox(height: 1,),
+              Container(
+                  height: 60,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius:
+                    BorderRadius.vertical(),
+                    color: const Color(0xffFFFFFF),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 8,
+                        offset: Offset(0, 4), // Shadow position
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Thư viện ảnh",
+                                  style: TextStyle(
+                                      fontSize: 16, fontFamily: "Lato"),
+                                ))),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 45),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: InkWell(
+                                onTap: () {
+                                  //Navigator.push(
+                                  //context,
+                                  //MaterialPageRoute(
+                                  // builder: (context) => Information()));
+                                },
+                                child: const Text(
+                                  "",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontFamily: "Lato"),
+                                ),
+                              ),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 15),
+                            child: Align(
+                                alignment: Alignment.centerRight,
+                                child: CupertinoSwitch(
+                                  value: Gallery ,
+                                  onChanged:(bool blackscreenvalue){
+                                    //khong biet cach setstate
+                                  } ,
+                                )))
+                      ],
+                    ),
+                  )),
+              SizedBox(height: 1,),
+              Container(
+                  height: 60,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(10)),
+                    color: const Color(0xffFFFFFF),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 8,
+                        offset: Offset(0, 4), // Shadow position
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  "Vị trí",
+                                  style: TextStyle(
+                                      fontSize: 16, fontFamily: "Lato"),
+                                ))),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 45),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: InkWell(
+                                onTap: () {
+                                  //Navigator.push(
+                                  //context,
+                                  //MaterialPageRoute(
+                                  // builder: (context) => Information()));
+                                },
+                                child: const Text(
+                                  "",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontFamily: "Lato"),
+                                ),
+                              ),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 15),
+                            child: Align(
+                                alignment: Alignment.centerRight,
+                                child: CupertinoSwitch(
+                                  value: Location ,
+                                  onChanged:(bool blackscreenvalue){
+                                    //khong biet cach setstate
+                                  } ,
+                                )))
+                      ],
+                    ),
+                  )),
+
             ],
           ),
         ));
